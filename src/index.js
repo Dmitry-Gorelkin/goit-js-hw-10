c5f247b1a636ddd0a55d7d10316bc4a067e0f191
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-01-17 23:19:35 +0200

    first_ver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,76 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+// import fetchCountries from './js/fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+
+function onInput(e) {
+  const country = e.target.value;
+  clear();
+  if (!country.trim()) {
+    return;
+  }
+  fetchCountries(country);
+}
+
+function fetchCountries(c) {
+  fetch(`https://restcountries.com/v2/name/${c}?fields=name,capital,population,flags,languages`)
+    .then(r => r.json())
+    .then(data => {
+      if (data.status === 404) {
+        throw new Error(response.status);
+      }
+      return data;
+    })
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.', {
+          timeout: 5000,
+        });
+      } else if (data.length < 2) {
+        oneCountry(data);
+      } else {
+        allCountry(data);
+      }
+    })
+    .catch(() => {
+      Notify.failure('Oops, there is no country with that name', {
+        timeout: 5000,
+      });
+    });
+}
+
+function oneCountry(arr) {
+  const country = arr
+    .map(({ name, population, capital, languages, flags: { svg } }) => {
+      const language = languages.map(e => e.name).join(', ');
+      return `<h2><img class="icon" src="${svg}" alt="flag" width="30"/>${name}</h2><p><span>Capital: </span>${capital}</p><p><span>Population: </span>${population}</p><p><span>Languages: </span>${language}</p>`;
+    })
+    .join('');
+
+  refs.list.insertAdjacentHTML('beforeend', country);
+}
+
+function allCountry(arr) {
+  const list = arr
+    .map(({ name, flags: { svg } }) => {
+      return `<li><img class="icon" src="${svg}" alt="flag" width="20" />${name}</li>`;
+    })
+    .join('');
+
+  refs.list.insertAdjacentHTML('beforeend', list);
+}
+
+function clear() {
+  refs.list.innerHTML = '';
+  refs.info.innerHTML = '';
+}
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,5 @@
+export default class fetchCountries {
+  constructor() {}
+
+  get;
+}
